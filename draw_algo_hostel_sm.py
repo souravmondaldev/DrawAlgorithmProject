9d32b0585d93d37041441f90a51fa6aa06f28975
Sourav Mondal <souravmondal0341@gmail.com>
2020-05-27 19:29:29 +0530

    Third Commit

--- draw_algo_hostel_sm.py
+++ draw_algo_hostel_sm.py
@@ -1,95 +1,88 @@
 			#------Draw Algorithm Implimentation--#
 			#-----Created By: Sourav Mondal-------#
-			#-----Created at: 21/05/2020----------#
+			#-----Created at: 26/05/2020----------#
 			#------ Roll No - 17IT8052------------#
-			#------overall complexity is O(n^2) as used bubble sort for sorting drawNumbers
-			#------which can be reduced with merge sort O(nlog(n))
 import random
 
-def generateDrawNumbers(drawNumList, size):         #Used For generating random number list used as draw numbers
-	while(len(drawNumList)!=size):
-		num=random.randint(1,size)
-		if num not in drawNumList:
-			#if not already exist then append else loop again
-			drawNumList.append(num)
-	print(drawNumList)
-	return drawNumList
-def sortDrawList(studentChoice, n):						#Used for sorting choice list by draw number,using bubble sort 
-	for i in range(len(studentChoice)):					#Time Complexity : O(n^2)
-		for j in range(i+1,len(studentChoice)):
-			#Compareing draw numbers
-			if studentChoice[i][n]>studentChoice[j][n]:
-				tempVar=studentChoice[j]
-				studentChoice[j]=studentChoice[i]
-				studentChoice[i]=tempVar
+def randomNumbers(Numbers, size):         #Used For generating random number list used as draw numbers
+	Num=Numbers
+	random.shuffle(Num)
+	return Num
+def sortDrawList(choiceWithDrawNum):						#Used for sorting choice list by draw number,using bubble sort 
+	choiceWithDrawNum.sort(key=lambda num:num[1])
 
-def printList(studentChoice):							#Used for Printing Student choice list
-	for i in range(len(studentChoice)):
-		print(studentChoice[i])
 		
-		
-def readStudentChoiceInputFile(studentChoice):			#This function Takes input from another file and stores it in the student choice list
-	textFile= open('draw_algorithm_input.txt','r') 		#Opening the input File(Must be in same location)
-	lineList=textFile.readlines()
-	for subList in lineList:				#Spliting each space seperated lines and converting into integers list
-		subList=subList.split()
-		for i, v in enumerate(subList):
-			if v.isdigit():
-				subList[i]=int(v)
+def generateStudentChoice(n):
+	subList=[]
+	for i in range(0,n):
+		tempList=[i+1 for i in range(n)]
+		randomNum=randomNumbers(tempList,n)
+		subList.append(randomNum)
+	choiceList=[]
+	for i in range(n):
+		string=""
+		for j in range(n):
+			if j==0:
+				string += str(subList[i][j])
 			else:
-				subList[i]=v
-		studentChoice.append(subList)
-
-def drawAlgoithm(studentChoice, finalAllotmentList):	#The main draw algorithm function
-	#drawNumList will store the draw numbers
-	drawNumList=[]
-	#allotedHostels will take care of how many hostels alloted so far
-	allotedHostels=[]
-	size=len(studentChoice)
-	#n is total number of elememnt in each list
-	n=len(studentChoice[0])
-	#generating draw numbers and inserting them at the end of the student choice list
-	generateDrawNumbers(drawNumList,size)
-	for i in range(len(studentChoice)):			
-		studentChoice[i].append(drawNumList[i])
-		
-	print("After inserting draw numbers to the choice list:")
-	printList(studentChoice)
-	print("-----")
-	#Sorting the student choice list according to  the draw numbers
-	sortDrawList(studentChoice,n)
-	print("Choice list after sorting according to the draw number:")
-	printList(studentChoice)
-	print("-----")
+				string += " "+str(subList[i][j])
+		choiceList.append(string)
+	print("Student Choice List:")
+	for i in range(n):
+		print(choiceList[i])
+	print(" ")
 	
-	#alloting rooms to the student according to the draw number priority and vacent rooms availble
-	for i in range(len(studentChoice)):
-		allotment=[]
-		allotment.append(studentChoice[i][0])
-		for j in range(1,n):			
-			#Alloting vacent room to the students and once they got a room break the loop
-			if studentChoice[i][j] not in allotedHostels:
-				allotedHostels.append(studentChoice[i][j])
-				allotment.append(studentChoice[i][j])
+	f=open('input.txt','w')
+	for i in range(n):
+		f.writelines(str(choiceList[i]))
+		f.write("\n")
+	
+
+def drawAlgoithm(studentChoice, finalAllotmentList,n):	#The main draw algorithm function
+	# #drawNumList will store the draw numbers
+	generateStudentChoice(n)
+	#choice=[]
+	inputChoices=[]
+	
+	inputFromFile=open('input.txt','r')
+	stringLine=inputFromFile.read()
+	inputChoices.append(stringLine.splitlines())
+	
+	for i in range(0,n):
+		tempVariable=[int(num) for num in inputChoices[0][i].split()]
+		studentChoice.append(tempVariable)
+	# drawNumList=[]
+	drawNumbers=[i+1 for i in range(n)]
+	drawNumbers=randomNumbers(drawNumbers,n)
+	print("Draw Numbers are:",drawNumbers[0:])
+	choiceWithDrawNum=[]
+	for i in range(0,n):
+		pair=(i,drawNumbers[i])
+		choiceWithDrawNum.append(pair)
+		
+	sortDrawList(choiceWithDrawNum)
+	alloted=[0]*(n+1)
+	
+	picked=0
+	for i in range(0,n):
+		roomNow=choiceWithDrawNum[i][0]
+		for j in range(0,n):
+			picked=studentChoice[roomNow][j]
+			if alloted[picked]==0:
+				finalAllotmentList[roomNow]=picked
+				alloted[picked]=1
 				break
-			else:
-				continue
-		finalAllotmentList.append(allotment)
-			
+				
+	print("Alloted Rooms to the student are:", finalAllotmentList)
 
 def main():
 	print("---------Implimentation of a Draw Algorithm for allotment of hostel room--------")
-	print("Total number of rooms in hostel is:15")
-	print("Total number of student needs room:15")
-	studentChoice=[];
-	readStudentChoiceInputFile(studentChoice)  #reading the input file
-	print("Student Preference to the hostel rooms are:")
-	printList(studentChoice)
-	finalAllotmentList=[]						#Final alloted list
+	studentChoice=[]
+	n=int(input("Enter the number of student to allot rooms:"))
+	finalAllotmentList=[0]*n						#Final alloted list
 	
-	drawAlgoithm(studentChoice,finalAllotmentList);
-	print("Final List of Allotment: ")
-	printList(finalAllotmentList)
+	drawAlgoithm(studentChoice,finalAllotmentList,n);
+	print("------End-----")
 
 if __name__=="__main__":
 	main()
